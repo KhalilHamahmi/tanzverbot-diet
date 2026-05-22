53b6d6d2f999b74ca3d43e8c272aa613bce20299
Khalil Hamahmi <khalil_hamahmi@sluz.ch>
2026-05-22 09:25:23 +0200

    Erste Änderungen

--- demo.ts
+++ demo.ts
@@ -1,9 +1,15 @@
 import { Sex, calcDateOnDiet } from "./tanzverbot_diet.ts";
 
 const fromWeight = 78.0;
-const toWeight = 100.0;
+const toWeight = 90.0;
 const height = 1.88;
 const age = 38;
 const sex = Sex.Male;
 const days = calcDateOnDiet(fromWeight, toWeight, height, age, sex);
-console.log(`You need to diet for ${days} days to reach your target.`);
+//console.log(`You need to diet for ${days} days to reach your target.`);
+const avarageWeightGainPerDay = (toWeight - fromWeight) / days;
+console.log(
+  `You need to diet for ${days} days to reach your target, with an avarage weight gain of ${avarageWeightGainPerDay.toFixed(
+    2,
+  )} kg per day.`,
+); 
--- tanzverbot_diet_test.ts
+++ tanzverbot_diet_test.ts
@@ -2,7 +2,7 @@ import { assertGreater } from "@std/assert";
 import { calcDateOnDiet, Sex } from "./tanzverbot_diet.ts";
 
 Deno.test("Tanzverbot Diet", () => {
-  assertGreater(calcDateOnDiet(74, 100, 1.86, 38, Sex.Male), 0.0);
+  assertGreater(calcDateOnDiet(74,80, 1.86, 38, Sex.Male), 0.0);
 });
 
 Deno.test("Tanzverbot Diet - Invalid Weight Loss", () => {
