f4f108734ecccc73cc86138fdd5a15ee7ddd61cf
Khalil Hamahmi <khalil_hamahmi@sluz.ch>
2026-05-22 08:46:44 +0200

    Erster Testfall

--- tanzverbot_diet_test.ts
+++ tanzverbot_diet_test.ts
@@ -4,3 +4,19 @@ import { calcDateOnDiet, Sex } from "./tanzverbot_diet.ts";
 Deno.test("Tanzverbot Diet", () => {
   assertGreater(calcDateOnDiet(74, 100, 1.86, 38, Sex.Male), 0.0);
 });
+
+Deno.test("Tanzverbot Diet - Invalid Weight Loss", () => {
+  try {
+    calcDateOnDiet(74, 70, 1.86, 38, Sex.Male);
+    throw new Error("Expected error was not thrown");
+  } catch (error) {
+    if (error instanceof Error) {
+      assertGreater(error.message.indexOf("loosing"), -1);    
+    } else {
+      throw error;
+    }
+  }
+});
+
+
+
